5ee6caa4a8ed137836c635d066761391aaa312a3
ceylankubi <tententwnytwny@gmail.com>
2026-01-25 11:01:20 +0300

    Update run.py
    denmflask

--- 📄 backend/run.py
+++ 📄 backend/run.py
@@ -1,1 +1,11 @@
 print("Flask çalışıyor")
+from app import create_app, db
+
+app = create_app()
+
+with app.app_context():
+    db.create_all()
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
