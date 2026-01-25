4f127d0296591654c6a9cdf73d770951e8a40a09
ceylankubi <tententwnytwny@gmail.com>
2026-01-25 10:56:48 +0300

    Update main.py
    dnmflask

--- backend/app/routes/main.py
+++ backend/app/routes/main.py
@@ -1,1 +1,9 @@
 print("Routes çalışıyor")
+from flask import Blueprint, render_template
+
+main_bp = Blueprint("main", __name__)
+
+
+@main_bp.route("/")
+def home():
+    return render_template("index.html")
