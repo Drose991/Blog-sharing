41c92f99b6abbbffa812f113b2323de8d198a5e8
ceylankubi <tententwnytwny@gmail.com>
2026-01-25 10:55:49 +0300

    Update __init__.py
    denemeflask

--- backend/app/__init__.py
+++ backend/app/__init__.py
@@ -1,1 +1,20 @@
 # Flask app init
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+
+db = SQLAlchemy()
+
+
+def create_app():
+    app = Flask(__name__)
+
+    app.config["SECRET_KEY"] = "secret-key"
+    app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///app.db"
+
+    db.init_app(app)
+
+    # Routes kaydı
+    from .routes.main import main_bp
+    app.register_blueprint(main_bp)
+
+    return app
