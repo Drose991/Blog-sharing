6714f59b44cfa75fb4b2edeb143ce22d94873c47
Dros <arda3344k@gmail.com>
2026-01-31 21:05:01 +0100

    Delete backend/app directory
    We decided to delete it

--- backend/app/routes/main.py
+++
@@ -1,9 +0,0 @@
-print("Routes çalışıyor")
-from flask import Blueprint, render_template
-
-main_bp = Blueprint("main", __name__)
-
-
-@main_bp.route("/")
-def home():
-    return render_template("index.html")
